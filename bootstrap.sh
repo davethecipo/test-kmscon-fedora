d5e1c63661a85a1db923416d98474946ecdab43c
Davide Olianas <ubuntupk@gmail.com>
2022-08-19 22:53:50 +0200

    disable systemctl service

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,26 +15,31 @@ git clone https://github.com/Aetf/kmscon
 cd /home/vagrant/kmscon
 chown -R vagrant kmscon
 meson build
+meson configure build -Dvideo_fbdev=disabled -Dfont_unifont=disabled -Drenderer_bbulk=disabled -Dextra_debug=true
 sudo meson install -C build/
 # install fish and oh-my-posh to test if glyphs are rendered
-#dnf install -y fish
-#sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-#sudo chmod +x /usr/local/bin/oh-my-posh
-#mkdir ~/.poshthemes
-#wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-#unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-#chmod u+rw ~/.poshthemes/*.omp.*
-#rm ~/.poshthemes/themes.zip
-#oh-my-posh font install FiraCode
+dnf install -y fish
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+oh-my-posh font install FiraCode
 # configure kmscon
-cp /usr/lib/systemd/system/kmsconvt\@.service /etc/systemd/system/
+# don't use systemd for now, because it introduces other problems; instead launches 
+# kmsconvt manually
+# cp /usr/lib/systemd/system/kmsconvt\@.service /etc/systemd/system/
 # this command is need to allow systemd launchig the kmscon executable
-chcon -R -t bin_t /usr/local/bin/kmscon
-mkdir /etc/kmscon
-echo """font-name=FiraCode Nerd Font Mono
-font-size=18
-drm
-hwaccel
-xkb-layout=en
-""" > /etc/kmscon/kmscon.conf
-systemctl enable --now kmsconvt@tty4.service
+# chcon -R -t bin_t /usr/local/bin/kmscon
+# mkdir /etc/kmscon
+# echo """font-name=FiraCode Nerd Font Mono
+# font-size=18
+# drm
+# hwaccel
+# xkb-layout=en
+# """ > /etc/kmscon/kmscon.conf
+#systemctl enable --now kmsconvt@tty4.service
+# manually run kmscon as root like this:
+kmsconvt --vt=tty4 --no-switchvt --debug --font-name="FiraCode Nerd Font Mono" --font-engine=pango --kwaccel &
